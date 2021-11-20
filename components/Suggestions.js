e403dadcca3710cb45f6b074cf77f5f648691d83
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-11-20 09:54:17 -0500

    suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -27,7 +27,16 @@ function Suggestions() {
       {
         suggestions.map(profile => (
           <div key={profile.id} className="flex items-center justify-between mt-3">
+
             <img className="w-10 h-10 border rounded-full p-[2px]" src={profile.avatar} alt="pic" />
+
+            <div className="flex-1 ml-4">
+              <h2 className="font-semibold text-sm">{profile.username}</h2>
+              <h3 className="text-xs text-gray-400">works at {profile.company.name}</h3>
+            </div>
+            
+            <button className="text-blue-400 text-xs font-bold">Follow</button>
+
           </div>
         ))
       }
