e01e5666d23d351d2bef6c96e2e12ed821d824f4
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-11-21 17:50:43 -0500

    fixed error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,12 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
   pages: {
     signIn: "/auth/signin",
